caaef2444242947acc1412ad44fcb3d4bb654c86
COR Students <corstudents@Matts-MBP-10.localdomain>
2018-10-22 12:16:42 -0400

    finishe assessment

---
+++ assessment.js
@@ -0,0 +1,64 @@
+// Question 1: Create a variable of each of the following data types
+// string,int,bool,float
+
+// string 
+let name = "Nyesha";
+// ing
+let age = "29";
+// bool
+if ("25" == "25") {
+    console.log(10);
+}
+// float
+let pi = 3.14
+
+// Question 2: create a variable called trueOrFalse. 
+// Create an if statement so that if the variable is the true the number 1 
+// is returned. Otherwise the number 2 is returned.
+let bigBlue =0;{
+    if (bigBlue === 1){
+    return 1 
+    } else if (bigBlue ===2); {
+        return 2;
+    }
+
+    // Question 3 Create a for loop that counts every even number
+    //  from 1-100 starting at 0
+
+    for (i = 0; 1<= 100 ; i++){
+        console.log(i);
+    }
+    for(i=0;i>0;i+2){
+        console.log(i)
+    }
+}
+
+
+
+// Question 4 Create a variable that is a bool and is equal to true
+// create a while loop that will print"hello"
+// to the console 20 times and then change that variable to false 
+// ending the loop
+let sign=0
+while(trueOrFalse){
+    console.log("hello");
+    sign++;
+if(sign===20){
+    trueOrFalse=false;
+}
+}
+// Question 5 create a function that can determine if a parameter is true
+// or false. If it is true return the string "true".
+// Otherwise,return the string "false".
+
+let favoriteMakeUp="fenty"
+if(favoriteMakeUp==="tooFaced"){
+    return "tooFaced";
+}
+else if(favoriteMakeUp==="huda"){
+    return "huda";
+}else if(favoriteMakeUp==="fenty"){
+    return "fenty";
+} 
+// Bonus question what does D.R.Y stand for?
+// Don't Repeat Yourself 
